3839ab33c75d4d4be1c7947c18d0c5e7d4d1d560
ParthipanRadhakrishnan <86599441+ParthipanRadhakrishnan@users.noreply.github.com>
2021-10-16 11:12:21 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var roof;
+//Create multiple bobs, mulitple ropes varibale here
+var bob1,bob2,bob3,bob4,bob5,rope
+
+
+function setup() {
+	createCanvas(800, 600);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var roof_options={
+		isStatic:true			
+	}
+	var bob1_options={
+		isStatic:false,
+		restitution:0.25,
+		density:0.8
+	}
+
+	roof = Bodies.rectangle(400,100,230,20,roof_options);
+    World.add(world,roof);
+
+	
+
+	bob1 = Bodies.circle(330,200,20,bob1_options);
+	World.add(world,bob1);
+	bob2 = Bodies.circle(360,200,20,bob1_options);
+	World.add(world,bob2);
+	bob3 = Bodies.circle(390,200,20,bob1_options);
+	World.add(world,bob3);
+	bob4 = Bodies.circle(420,200,20,bob1_options);
+	World.add(world,bob4);
+	bob5 = Bodies.circle(450,200,20,bob1_options);
+	World.add(world,bob5);
+
+	rope1 = new Rope(bob1,roof,-70,0);
+	rope2 = new Rope(bob2,roof,-40,0);
+	rope3 = new Rope(bob3,roof,-10,0);
+	rope4 = new Rope(bob4,roof,20,0);
+	rope5 = new Rope(bob5,roof,50,0);
+
+	Engine.run(engine);
+	
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  background("#99004d");
+  Engine.update(engine);
+
+  rect(roof.position.x,roof.position.y,230,20);
+
+  //call display() to show ropes here
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  
+  //create ellipse shape for multiple bobs here
+ellipse(bob1.position.x,bob1.position.y,20);
+ellipse(bob2.position.x,bob2.position.y,20);
+ellipse(bob3.position.x,bob3.position.y,20);
+ellipse(bob4.position.x,bob4.position.y,20);
+ellipse(bob5.position.x,bob5.position.y,20);
+}
+
+//Write keyPressed function and apply force on pressing up_arrow key on the first bob.
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(bob1,bob1.position,{x:-10,y:-100})
+	}
+}
